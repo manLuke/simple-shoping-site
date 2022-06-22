0139a4099e0fb706469cbbaccf34d858f8fe03ad
tomas_kacha <kachatomas@gmail.com>
2022-06-22 23:39:38 +0200

    add nuxt version - not working yet

---
+++ nuxt/pekarna.cz/app.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <navbar />
+    <div id="page">
+        <NuxtPage/>
+    </div>
+  </div>
+</template>
+
+<style>
+#page {
+  padding: 15vh 0 5vh 0;
+}
+</style>
---
+++ nuxt/pekarna.cz/pages/index.vue
@@ -0,0 +1,45 @@
+<template>
+<div class="index">
+    <h2 id="index-title">Vyberte si z našeho zboží.</h2>
+    <div class="mainPage">
+      <div class="mainPage-products">
+        <singleProduct v-for="item in getAllProducts" :key="item.id" :product="item"/>
+      </div>
+    </div>
+</div>
+</template>
+
+<script>
+
+import { mapState } from 'pinia'
+import { useStore } from '@/store/main'
+
+export default {
+  name: 'index',
+  computed: {
+    getAllProducts () {
+      return {...mapState(useStore, ['getAllProducts'])}
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.index {
+  text-align: center;
+  #index-title {
+    font-size: 2em;
+    margin: 1rem;
+  }
+}
+
+.mainPage-products {
+  display: flex;
+  gap: 1em;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 0 auto;
+  width: 100%;
+  max-width: 1200px;
+}
+</style>
